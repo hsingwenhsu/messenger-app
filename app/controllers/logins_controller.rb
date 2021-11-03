5061b23358360b07a560f182d4376ff65074daf6
YowKuan <yoyo6213@gmail.com>
2021-11-02 23:16:46 -0400

    completed users test

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,24 +1,24 @@
-class LoginsController < ApplicationController
-    def new
-    end
+# class LoginsController < ApplicationController
+#     def new
+#     end
   
-    def create
-      if user = authenticate_with_google
-        cookies.signed[:user_id] = user.id
-        #redirect_to user
-        redirect_to "http://localhost:3000/rooms", alert: 'authentication_succeeded'
-      else
-        redirect_to "http://localhost:3000/"
-      end
-    end
+#     def create
+#       if user = authenticate_with_google
+#         cookies.signed[:user_id] = user.id
+#         #redirect_to user
+#         redirect_to "http://localhost:3000/rooms", alert: 'authentication_succeeded'
+#       else
+#         redirect_to "http://localhost:3000/"
+#       end
+#     end
   
-    private
-      def authenticate_with_google
-        if id_token = flash[:google_sign_in][:id_token]
-          User.find_by google_id: GoogleSignIn::Identity.new(id_token).user_id
-        elsif error = flash[:google_sign_in][:error]
-          logger.error "Google authentication error: #{error}"
-          nil
-        end
-      end
-  end
+#     private
+#       def authenticate_with_google
+#         if id_token = flash[:google_sign_in][:id_token]
+#           User.find_by google_id: GoogleSignIn::Identity.new(id_token).user_id
+#         elsif error = flash[:google_sign_in][:error]
+#           logger.error "Google authentication error: #{error}"
+#           nil
+#         end
+#       end
+#   end
---
+++ features/user_action.feature
@@ -0,0 +1,38 @@
+Feature: Users actions
+Scenario: Users want to see their profile
+Given I created a user
+And I am on the users page
+When I clicked show
+Then I should see edit and back
+When I clicked back
+Then I should go back to users page
+
+Scenario: Users want to delete their profile
+Given I created a user
+And I am on the users page
+When I clicked destroy
+Then I see a message that I successfully deleted the user
+Then I should not see user on the page
+
+Scenario: Users want to update their profile
+Given I created a user
+And I am on the users page
+When I clicked edit
+Then I should see Update User
+When I clicked Update User
+Then I should see User was successfully updated.
+
+Scenario: Users want to create new user for themselves
+Given I am on the users page
+And I clicked New User
+Then I should see create user button
+And I clicked create user
+Then I should see User was successfully created.
+When I clicked back
+And I clicked New User
+And I clicked create user
+Then I should see Username has already been taken
+
+
+
+
