cc7ffa5703f881743b1e27304e8fbdee539cc05a
hsingwenhsu <b05502012@ntu.edu.tw>
2021-11-02 20:30:22 -0400

    gemfiles

--- features/chatting_steps.rb
+++ features/chatting_steps.rb
@@ -2,3 +2,26 @@ Given('I am on the home page') do
   #pending # Write code here that turns the phrase above into concrete actions
   visit '/rooms'
 end
+
+Given /the following rooms are created: (.*)/ do |rooms|
+  rooms.split(",").each do |room|
+    puts(room)
+  end
+  puts(page.body)
+end
+
+Then /^I should see the following rooms: (.*)/ do |rooms|
+  rooms.split(",").each do |room|
+    if page.respond_to? :should
+      expect(page).to have_content(room)
+    else
+      assert expect(page).to has_content?(room)
+    end
+  end
+end
+
+Then /^I press the room of (.*)$/ do |link|
+  #find(link).click_link(link)
+  expect(page).to have_content(link)
+  click_link(class: link)
+end
