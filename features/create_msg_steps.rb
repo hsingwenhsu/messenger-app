8f0cf7872fc13d65ee57e802bf394299e73e658b
hsingwenhsu <b05502012@ntu.edu.tw>
2021-11-17 13:05:56 -0500

    modify room index.html

--- features/create_msg_steps.rb
+++ features/create_msg_steps.rb
@@ -1,17 +1,17 @@
-Then('I create a message Hehehe') do
-    fill_in 'msg_input', :with => 'Hehehe'
-    click_button('msg_submit')
+Then('I create a message whyyyyy') do
+    fill_in 'msg-input', :with => 'whyyyyy'
+    click_button('msg-submit')
     visit '/rooms/1'
 end
   
-Then('I should see a message Hehehe') do
-    page.should have_content('Hehehe')
+Then('I should see a message whyyyyy') do
+    page.should have_content('whyyyyy')
 end
 
-And('I click the delete button of Hehehe') do 
+And('I click the delete button of whyyyyy') do 
     click_link('1')
 end
 
-Then('I should not see Hehehe') do
-    page.should !have_content('Hehehe')
+Then('I should not see whyyyyy') do
+    page.should !have_content('whyyyyy')
 end
