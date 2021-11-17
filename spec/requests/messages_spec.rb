06cfdec4c4d06a8e824592435bfa51c874dd8433
chiajoukuo <b05901166@ntu.edu.tw>
2021-11-17 08:53:16 -0500

    message rspec

--- spec/requests/messages_spec.rb
+++ spec/requests/messages_spec.rb
@@ -33,26 +33,46 @@ RSpec.describe "Messages", type: :request do
     #   :avatar_url => 'test_url'
     #   # etc.
     # })
-    admin = Admin.create!(:uid => "111101028321895", :email => '2@columbia.edu', :full_name => 'Test')
+    # Capybara.default_host = 'localhost:3000'
+    # puts default_url_options
+    default_url_options[:host] = 'localhost:3000'
+    # puts default_url_options
+    email_length = rand(20...50)
+    email = (0...email_length).map { ('a'..'z').to_a[rand(26)] }.join+'@columbia.edu'
+    admin = Admin.create!(:uid => "111101028321895", :email => email, :full_name => 'Test')
+    # puts 'email'
+    # puts email
+    # puts email_length
     # user = Admin.find_by(uid: "111101028321895")
     login_as(admin, :scope => :admin)
     current_admin = admin
-    # puts 12341234
+    
 
   end
 
   describe "GET /index" do
     it "renders a successful response" do
+      
+      # @request.host = localhost:3000
+      # puts @request.host
       Message.create!(valid_attributes)
-      get messages_url
+      get "/messages" # , headers: { "HTTP_REFERER": "localhost:3000" }
+      # puts response.header
+      # puts response.body
       expect(response).to be_successful
+      
     end
   end
 
   describe "GET /show" do
     it "renders a successful response" do
       message = Message.create!(valid_attributes)
-      get message_url(message)
+      # get message_url(message)
+      # puts 1234
+      # puts message_url(message) # .http://localhost/messages/1
+      get '/messages/1'
+      # puts response.header
+      # puts response.body
       expect(response).to be_successful
     end
   end
@@ -75,8 +95,11 @@ RSpec.describe "Messages", type: :request do
   describe "POST /create" do
     context "with valid parameters" do
       it "creates a new message" do
+        # puts messages_url # .http://localhost/messages
+        # puts message_url # http://localhost/messages/1
+        
         expect {
-          post messages_url, params: { message: valid_attributes }
+          post '/messages', params: { message: valid_attributes }
         }.to change(Message, :count).by(1)
       end
 
@@ -89,7 +112,7 @@ RSpec.describe "Messages", type: :request do
     context "with invalid parameters" do
       it "does not create a new message" do
         expect {
-          post messages_url, params: { message: invalid_attributes }
+          post '/messages', params: { message: invalid_attributes }
         }.to raise_error(ActiveJob::SerializationError)
       end
       
@@ -104,14 +127,14 @@ RSpec.describe "Messages", type: :request do
     it "destroys the requested message" do
       message = Message.create!(valid_attributes)
       expect {
-        delete message_url(message)
+        delete '/messages/1'
       }.to change(Message, :count).by(-1)
     end
 
     it "redirects to the messages list" do
       message = Message.create!(valid_attributes)
-      delete message_url(message)
-      expect(response).to redirect_to(messages_url)
+      delete '/messages/1'
+      expect(response).to redirect_to('/messages')
     end
   end
 end
