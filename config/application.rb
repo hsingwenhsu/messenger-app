6bc4be2c75c5cb39b151fb56c101e6208f817f2e
hsingwenhsu <b05502012@ntu.edu.tw>
2021-11-17 17:04:32 -0500

    config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -5,13 +5,13 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.autoloader = :classic
+
 
 module ChatRoom
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
-
+    # config.load_defaults 6.0
+    config.autoloader = :classic
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
