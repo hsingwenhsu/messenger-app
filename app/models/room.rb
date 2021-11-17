5a2185bfe4aaa151ed62fe5bba20c4350cd23d47
hsingwenhsu <b05502012@ntu.edu.tw>
2021-11-17 15:28:57 -0500

    fail to fix delete room

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,6 @@
 class Room < ApplicationRecord
   has_many :messages
-  
+  has_many :admins
   has_many :admins, through: :messages # test for google user
   has_many :admins, through: :enrollments
 end
