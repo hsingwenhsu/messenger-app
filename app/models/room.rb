141470f787aac3ed8a237ab55248365fd55fc87d
dyadi <dyadi0413@gmail.com>
2021-12-03 12:05:16 -0500

    room sidebar

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,7 @@
 class Room < ApplicationRecord
   has_many :messages
   has_many :admins
+  has_many :posts, -> { order 'updated_at desc' }
   has_many :admins, through: :messages # test for google user
   has_many :admins, through: :enrollments
 end
