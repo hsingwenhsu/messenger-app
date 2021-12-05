0ac5e97bc63e8db045eb7b7b02d121bf8ea72634
hsingwenhsu <b05502012@ntu.edu.tw>
2021-12-05 04:44:14 -0500

    fix private refresh

--- db/migrate/20211203220814_add_room_id_to_events.rb
+++
@@ -1,7 +0,0 @@
-class AddRoomIdToEvents < ActiveRecord::Migration[6.0]
-  def change
-    add_column :events, :room_id, :integer
-    add_foreign_key :events, :rooms, column: :room_id
-    add_index :events, :room_id
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_03_220814) do
+ActiveRecord::Schema.define(version: 2021_12_03_015728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,11 +56,11 @@ ActiveRecord::Schema.define(version: 2021_12_03_220814) do
   create_table "events", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "room_id"
     t.string "title"
     t.date "event_date"
     t.datetime "event_start"
     t.datetime "event_end"
-    t.integer "room_id"
     t.index ["room_id"], name: "index_events_on_room_id"
   end
 
@@ -112,7 +112,6 @@ ActiveRecord::Schema.define(version: 2021_12_03_220814) do
   add_foreign_key "attendances", "events"
   add_foreign_key "event_messages", "admins"
   add_foreign_key "event_messages", "events"
-  add_foreign_key "events", "rooms"
   add_foreign_key "posts", "admins"
   add_foreign_key "posts", "rooms"
   add_foreign_key "private_messages", "admins"
