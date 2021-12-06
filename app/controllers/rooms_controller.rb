446f689cfbfb4c2873dfe2949c2cde3d80e60cab
hsingwenhsu <b05502012@ntu.edu.tw>
2021-12-06 02:58:04 -0500

    search function

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,7 +4,8 @@ class RoomsController < ApplicationController
   # GET /rooms
   # GET /rooms.json
   def index
-    @rooms = Room.all
+    # @rooms = Room.all
+    @rooms = Room.search(params[:search])
   end
 
   # def search
--- app/models/room.rb
+++ app/models/room.rb
@@ -5,4 +5,12 @@ class Room < ApplicationRecord
   has_many :events
   has_many :admins, through: :messages # test for google user
   has_many :admins, through: :enrollments
+  def self.search(search)
+    if search.blank?
+      @rooms = Room.all
+    else
+      @search_param = search.downcase
+      @rooms = Room.all.where("lower(name) LIKE :search", search: "%#{@search_param}%")
+    end
+  end
 end
