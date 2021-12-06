adeb9c88beaa1b2cc51cce61d75df48ede8155ec
hsingwenhsu <b05502012@ntu.edu.tw>
2021-12-06 15:02:26 -0500

    delete room cascade

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,10 +1,11 @@
 class Room < ApplicationRecord
   has_many :messages
   has_many :admins
-  has_many :posts, -> { order 'updated_at desc' }
+  has_many :posts, -> { order 'updated_at desc' }, dependent: :delete_all
   has_many :events
   has_many :admins, through: :messages # test for google user
   has_many :admins, through: :enrollments
+  
   def self.search(search)
     if search.blank?
       @rooms = Room.all
